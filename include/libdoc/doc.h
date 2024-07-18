e53b5d7044a7ccf4b87d5fdce7f06308171ffefb
Igor V. Sementsov <ig.kuzm@gmail.com>
2024-07-18 21:52:12 +1000

    MS DOC table borders

--- include/libdoc/doc.h
+++ include/libdoc/doc.h
@@ -2,7 +2,7 @@
  * File              : doc.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 04.11.2022
- * Last Modified Date: 30.05.2024
+ * Last Modified Date: 18.07.2024
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
@@ -13,7 +13,7 @@
 extern "C"{
 #endif
 
-#define DEBUG
+//#define DEBUG
 
 #include <stdint.h>
 #include <stdio.h>
